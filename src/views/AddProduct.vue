ac7f595d2c296fc2e9962851dd30bcd8fdbf5f7e
Nathan Sadala <bernynath@gmail.com>
2021-08-07 01:07:56 +0200

    addproduct v1

--- src/views/AddProduct.vue
+++ src/views/AddProduct.vue
@@ -7,17 +7,129 @@
         </v-btn>
       </v-col>
     </v-row>
+    <v-row class="mb-2">
+      <v-col offset-lg="3" lg="6" md="6" cols="12">
+        <v-card rounded="lg" class="pt-3 pb-3 pl-3 pr-3">
+          <v-card-title
+            class="d-flex justify-center text-center text-lg-h4 text-md-h4 text-md-h6"
+            >les informations du produit</v-card-title
+          >
+          <v-divider></v-divider>
+          &nbsp;
+          <v-form>
+            <v-text-field
+              rounded
+              outlined
+              label="Veuillez saisir le nom"
+            ></v-text-field>
+            <v-textarea
+              rounded
+              label="Veuillez saisir une description"
+              outlined
+            ></v-textarea>
+            <v-file-input
+              :rules="rules"
+              accept="image/png, image/jpeg, image/bmp, image/svg"
+              label="Veuillez uploader un fichier"
+              v-model="files"
+              multiple
+              chips
+              outlined
+              rounded
+              clearable
+            ></v-file-input>
+            <v-text-field
+              rounded
+              outlined
+              label="Veuillez saisir le prix de vente"
+            ></v-text-field>
+            <v-text-field
+              rounded
+              outlined
+              label="Veuillez saisir le prix pour avec une reduction (optionnelle)"
+            ></v-text-field>
+            <v-text-field
+              rounded
+              outlined
+              label="Veuillez saisir quantité disponible"
+            ></v-text-field>
+            <v-checkbox
+              v-model="checkbox"
+              label="Ce produit est disponible en plusieurs option(version, taille)"
+              @change="initArray"
+            ></v-checkbox>
+            <v-alert v-if="checkbox" type="info" dense
+              >Veuillez saisir les options :</v-alert
+            >
+            <v-btn-toggle
+              v-show="checkbox"
+              v-for="(option, i) in options"
+              :key="i"
+            >
+              <v-text-field
+                v-model="option.title"
+                outlined
+                dense
+                :placeholder="`Option ${i}`"
+              ></v-text-field>
+              &nbsp;
+              <v-text-field
+                v-model="option.description"
+                outlined
+                dense
+              ></v-text-field>
+              &nbsp;
+              <v-btn text icon small class="mt-1" rounded @click="deleteOption">
+                <v-icon color="red">mdi-window-close</v-icon>
+              </v-btn>
+            </v-btn-toggle>
+            <br />
+            <v-btn v-if="checkbox" rounded color="danger" @click="addOption">
+              <v-icon>mdi-plus</v-icon> Ajouter une option
+            </v-btn>
+          </v-form>
+          <v-card-actions>
+            <v-btn block rounded color="success">Ajouter</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
 export default {
+  delimiters: ["${", "}"],
   data: () => ({
     previousRoute: null,
+    checkbox: false,
+    options: [],
+    filelist: [],
+    files: [],
+    rules: [
+      (value) =>
+        !value || value.size < 2000000 || "la photo ne doit pas depasser 2 Mo!",
+    ],
   }),
   methods: {
     previous() {
       this.$router.push(this.previousRoute);
+    },
+    initArray() {
+      if (this.options.length === 0) {
+        this.options.push({ title: "", description: "" });
+      } else {
+        this.options = [];
+      }
+    },
+    addOption() {
+      this.options.push({ title: "", description: "" });
+    },
+    deleteOption() {
+      this.options.splice(0, 1);
+      if (this.options.length === 0) {
+        this.checkbox = false;
+      }
     },
   },
   beforeRouteEnter(to, from, next) {
@@ -28,5 +140,4 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
