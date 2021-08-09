371c8e579dca1f633dd235d59564824d8f513b41
Nathan Sadala <bernynath@gmail.com>
2021-08-09 21:04:33 +0200

    contact page

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,5 +1,23 @@
 <template>
-  <div class="about">
-    <h1>This is an contact page</h1>
+  <div>
+    <v-row class="mx-auto mt-3">
+      <v-col offset-lg="2" lg="8" cols="12">
+        <v-card flat>
+          <v-card-title class="d-flex justify-center text-lg-h2 text-h3">
+            Nous Contacter
+          </v-card-title>
+          <v-card-text>
+            <v-form>
+              <v-text-field outlined placeholder="Votre Nom"></v-text-field>
+              <v-text-field outlined placeholder="Votre Postnom"></v-text-field>
+              <v-text-field outlined placeholder="Votre Email"></v-text-field>
+              <v-textarea outlined placeholder="Votre Préoccupation"></v-textarea>
+              <v-btn rounded color="success" large block>Envoyer</v-btn>
+            </v-form>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col> </v-col>
+    </v-row>
   </div>
 </template>
